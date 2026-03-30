363e27904ded002f40379a4ca7f4ac4bc08db807
Majharulislam <majhurulislam535@gmail.com>
2026-03-30 22:05:28 +0600

    zod schema added

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { UserControllers } from "./user.controllers";
+import { validateRequest } from "../../middleware/validateRequest";
+import { createUserZodSchema } from "./user.validation";
 
 
 
 export const userRouter = Router(); 
 
-userRouter.post('/register',UserControllers.createUserControllers);
-userRouter.get("/",UserControllers.getAllUser)
+userRouter.post('/register',validateRequest(createUserZodSchema) ,UserControllers.createUserControllers);
+userRouter.get("/",UserControllers.getAllUser);
+
