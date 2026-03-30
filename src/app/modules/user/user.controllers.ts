aecfc30cbab4b3cd9373c96940956328d24f07e5
Majharulislam <majhurulislam535@gmail.com>
2026-03-30 19:16:10 +0600

    catch async function create

--- src/app/modules/user/user.controllers.ts
+++ src/app/modules/user/user.controllers.ts
@@ -1,27 +1,20 @@
 import { Request, Response } from "express";
 import { userService } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
 
 
-const createUserControllers = async (req: Request, res: Response,next:NewableFunction) => {
+const createUserControllers = catchAsync(async (req: Request, res: Response) => {
+
+     const user = await userService.createUserService(req.body);
+
+     res.status(201).json({
+          status: true,
+          message: "successfully user Created",
+          user
+     })
 
 
-
-
-     try {
-
-          const user = await userService.createUserService(req.body);
-
-          res.status(201).json({
-               status: true,
-               message: "successfully user Created",
-               user
-          })
-
-     } catch (err) {
-             next(err);
-     }
-
-}
+})
 
 
 
