8453c6006deb823e317c7d0c5802355d4f55c892
Majharulislam <majhurulislam535@gmail.com>
2026-03-30 20:46:41 +0600

    get all user and sendResponse function complete

--- src/app/modules/user/user.controllers.ts
+++ src/app/modules/user/user.controllers.ts
@@ -1,25 +1,42 @@
 import { Request, Response } from "express";
 import { userService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendRespons";
+import { CREATED, OK } from "http-status-codes";
 
 
 const createUserControllers = catchAsync(async (req: Request, res: Response) => {
 
      const user = await userService.createUserService(req.body);
 
-     res.status(201).json({
-          status: true,
-          message: "successfully user Created",
-          user
-     })
+    sendResponse(res, {
+        success: true,
+        statusCode:  CREATED,
+        message: "User Created Successfully",
+        data: user,
+    })
 
 
 })
 
 
+const getAllUser = catchAsync(async(req:Request,res:Response)=>{
+       const result = await userService.getAllUserService();
+       
+       sendResponse(res, {
+        success: true,
+        statusCode: OK,
+        message: "User Finded Successfully",
+        data:result.data ,
+        meta:result.meta
+    })
+       
+})
+
 
 
 
 export const UserControllers = {
-     createUserControllers
+     createUserControllers,
+     getAllUser
 }
