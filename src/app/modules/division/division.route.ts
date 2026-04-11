2b68600ec1afc4edec50d9648292a406d6e6dda9
Majharulislam <majhurulislam535@gmail.com>
2026-04-11 22:41:46 +0600

    added zod validation for division

--- src/app/modules/division/division.route.ts
+++ src/app/modules/division/division.route.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import { checkAuth } from "../../middleware/checkAuth";
 import { Role } from "../user/user.interface";
 import { divisionControllers } from "./division.controllers";
+import { validateRequest } from "../../middleware/validateRequest";
+import { createDivisionSchema } from "./division.validation";
 
 
 
 export const divisionRoute = Router();
 
 
-divisionRoute.post("/create",checkAuth(Role.ADMIN,Role.SUPER_ADMIN),divisionControllers.createDivision);
+divisionRoute.post("/create", validateRequest(createDivisionSchema),checkAuth(Role.ADMIN,Role.SUPER_ADMIN),divisionControllers.createDivision);
 
 
