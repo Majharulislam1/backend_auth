63e3aabc2e8850899526e4b12782464ca2fd3347
Majharulislam <majhurulislam535@gmail.com>
2026-04-11 22:21:03 +0600

    create division route created

--- src/app/modules/division/division.route.ts
+++ src/app/modules/division/division.route.ts
@@ -1,6 +1,13 @@
 import { Router } from "express";
+import { checkAuth } from "../../middleware/checkAuth";
+import { Role } from "../user/user.interface";
+import { divisionControllers } from "./division.controllers";
 
 
 
 export const divisionRoute = Router();
+
+
+divisionRoute.post("/create",checkAuth(Role.ADMIN,Role.SUPER_ADMIN),divisionControllers.createDivision);
+
 
