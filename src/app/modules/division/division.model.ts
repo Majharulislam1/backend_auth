ec7acb633fd97e66b001e227e8d707b460365348
Majharulislam <majhurulislam535@gmail.com>
2026-04-13 20:48:30 +0600

    added pre hooks for slug

--- src/app/modules/division/division.model.ts
+++ src/app/modules/division/division.model.ts
@@ -34,7 +34,39 @@ divisionSchema.pre<IDivision>("save", async function (this:IDivision) {
 });
  
 
+
  
+ 
+
+divisionSchema.pre("findOneAndUpdate", async function () {
+    const update = this.getUpdate() as { $set?: Partial<IDivision> } & Partial<IDivision>;
+
+    // Handle both { name: "..." } and { $set: { name: "..." } }
+    const name = update?.$set?.name || update?.name;
+
+    if (name) {
+        const baseSlug = `${name.toLowerCase().split(" ").join("-")}-division`;
+
+        let slug = baseSlug;
+        let counter = 1;
+
+        // Get the _id of the document being updated to exclude it
+        const filter = this.getFilter();
+
+        while (await Division.exists({ slug, _id: { $ne: filter._id } })) {
+            slug = `${baseSlug}-${counter++}`;
+        }
+
+        // Set slug in the correct place
+        if (update.$set) {
+            update.$set.slug = slug;
+        } else {
+            update.slug = slug;
+        }
+
+        this.setUpdate(update);
+    }
+});
 
 
 
